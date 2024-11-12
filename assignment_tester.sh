90374069f130d1f6c23f1d2fc5e4aab5d18dc16f
Samantha Norrie <49314677+Samantha-norrie@users.noreply.github.com>
2024-11-12 12:41:13 -0800

    Add files via upload

--- assignment_tester.sh
+++ assignment_tester.sh
@@ -3,24 +3,28 @@
 for i in {1..20}; do
     echo "Running iteration $i..."
 
-    command="./"
-    output="../tests/$i"
+    command_stub="./STUDENT_FILE"
 
-    if (( i % 2 == 0 )); then
-        command="$command summary"
-        output="${output}_summary.txt"
+    summary_command="$command_stub route_summary ../tests/$i.txt"
+    summary_output="../tests/${i}_summary.txt"
+
+    days_command="$command_stub days ../tests/$i.txt"
+    days_output="../tests/${i}_days.txt"
+
+
+    actual_summary_output=$($summary_command)
+    actual_days_output=$($days_command)
+
+
+    if diff <(echo "$actual_summary_output") "$summary_output"; then
+        echo "SUMMARY Iteration $i: Output matches expected output."
     else
-        command="$command days"
-        output="${output}_days.txt"
+        echo "SUMMARY Iteration $i: Output differs from expected output."
     fi
 
-    command="$command ../tests/$i.txt"
-
-    actual_output=$($command)
-
-    if diff <(echo "$actual_output") "$output"; then
-        echo "Iteration $i: Output matches expected output."
+    if diff <(echo "$actual_days_output") "$days_output"; then
+        echo "DAYS Iteration $i: Output matches expected output."
     else
-        echo "Iteration $i: Output differs from expected output."
+        echo "DAYS Iteration $i: Output differs from expected output."
     fi
 done
